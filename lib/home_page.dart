729b338bc4ccb87ca80c9cfdfff3e5875b63d43e
alpjamal <mr.abdujamol1@mail.ru>
2023-09-12 18:42:04 +0500

    create model from json

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:ulesson_http/models/currency_model.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -8,34 +12,62 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static const String apiKey = '42db92acba22072dbc39cb83';
+  Currency _targetCurrency = Currency(name: 'USD', exchangeValue: 1);
+  List<Currency> _currencies = [];
+  bool _isLoading = false;
+
+  _loadData() async {
+    setState(() => _isLoading = true);
+
+
+
+    Uri url = Uri.parse('https://v6.exchangerate-api.com/v6/$apiKey/latest/${_targetCurrency.name}');
+
+
+    final response = await http.get(url);
+    Map<String, dynamic> map = jsonDecode(response.body);
+    CurrencyResponse currencyResponse = CurrencyResponse.fromJson(map);
+    _currencies = currencyResponse.conversionRates ?? [];
+
+
+
+    setState(() => _isLoading = false);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text(''),
+        title: Text(_targetCurrency.name),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: List.generate(
-            1,
-            (index) {
-              return const Card(
-                elevation: 1,
-                child: ListTile(
-                  title: Text('Usd'),
-                  trailing: Text(
-                    '0',
-                    style: TextStyle(fontSize: 18),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator.adaptive())
+          : _currencies.isEmpty
+              ? const Center(child: Text('No Data!'))
+              : SingleChildScrollView(
+                  child: Column(
+                    children: List.generate(
+                      _currencies.length,
+                      (index) {
+                        Currency currency = _currencies[index];
+                        return Card(
+                          elevation: 1,
+                          child: ListTile(
+                            title: Text(currency.name),
+                            trailing: Text(
+                              currency.exchangeValue.toStringAsFixed(2),
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
                   ),
                 ),
-              );
-            },
-          ),
-        ),
-      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () => _loadData(),
         child: const Icon(Icons.api),
       ),
     );
--- lib/models/currency_model.dart
+++ lib/models/currency_model.dart
@@ -1,9 +1,47 @@
+class CurrencyResponse {
+  String? result;
+  String? documentation;
+  String? termsOfUse;
+  num? timeLastUpdateUnix;
+  String? timeLastUpdateUtc;
+  num? timeNextUpdateUnix;
+  String? timeNextUpdateUtc;
+  String? baseCode;
+  List<Currency>? conversionRates;
+
+  CurrencyResponse.fromJson(Map<String, dynamic> json) {
+    result = json['result'];
+    documentation = json['documentation'];
+    termsOfUse = json['terms_of_use'];
+    timeLastUpdateUnix = json['time_last_update_unix'];
+    timeLastUpdateUtc = json['time_last_update_utc'];
+    timeNextUpdateUnix = json['time_next_update_unix'];
+    timeNextUpdateUtc = json['time_next_update_utc'];
+    baseCode = json['base_code'];
+
+    if (json['conversion_rates'] != null) {
+      conversionRates = [];
+      (json['conversion_rates'] as List).forEach((element) {
+        conversionRates!.add(
+          Currency(
+            name: element['name'],
+            exchangeValue: element['rate'],
+            time: element['time'],
+          ),
+        );
+      });
+    }
+  }
+}
+
 class Currency {
   final String name;
   final num exchangeValue;
+  final String time;
 
   Currency({
     required this.name,
     required this.exchangeValue,
+    required this.time,
   });
 }
